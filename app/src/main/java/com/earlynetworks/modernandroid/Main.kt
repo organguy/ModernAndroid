0d896516d26de293997acc7cb49c5c3452b04ab6
organguy <organguy@naver.com>
2021-07-01 07:04:41 +0900

    --- Kotlin 문법 ---   - 변수, 상수, 기본자료형, 메서드

--- app/src/main/java/com/earlynetworks/modernandroid/Main.kt
+++ app/src/main/java/com/earlynetworks/modernandroid/Main.kt
@@ -1,5 +1,12 @@
 package com.earlynetworks.modernandroid
 
 fun main(){
-    print("Hello World")
+    var x = 10
+    var str = "Hello"
+    var isMarried = true
+
+    println("$str World")
+    println(myMethod(10, 20))
 }
+
+fun myMethod(a: Int, b: Int): Int = a + b
