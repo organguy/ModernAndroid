4f2c14abe51b515006179a434c531f4563171391
organguy <organguy@naver.com>
2021-07-04 05:22:27 +0900

    --- Kotlin 문법 ---   - Unit, 배열, 리스트

--- app/src/main/java/com/earlynetworks/modernandroid/Main.kt
+++ app/src/main/java/com/earlynetworks/modernandroid/Main.kt
@@ -7,6 +7,16 @@ fun main(){
 
     println("$str World")
     println(myMethod(10, 20))
+
+    val items = listOf(1, 2, 3, 4, 5) // 불변
+    println(items)
+
+    val items2 = arrayListOf(1, 2, 3, 4, 5) // 변경 가능
+    items2.add(6)
+    items2.remove(1)
+    items2[0] = 10
+
+    println(items2)
 }
 
 fun myMethod(a: Int, b: Int): Int = a + b
