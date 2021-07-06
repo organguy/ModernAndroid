154f5bf69a6bd9c1bc5a98d09a8e6cff8f9d09e3
organguy <organguy@naver.com>
2021-07-07 07:11:41 +0900

    --- Kotlin 문법 ---   - 클래스

--- app/src/main/java/com/earlynetworks/modernandroid/Main.kt
+++ app/src/main/java/com/earlynetworks/modernandroid/Main.kt
@@ -17,6 +17,38 @@ fun main(){
     items2[0] = 10
 
     println(items2)
+
+    val xv = 2
+    val isEven = if(xv %2 == 0) "짝" else "홀"
+    println(isEven)
+
+    for(i in 0..9){
+        println(i)
+    }
+
+    val numbers = listOf(1, 2, 3, 4, 5)
+    for(i in numbers){
+        println(i)
+    }
+
+    var xw = 1
+
+    when (xw) {
+        1 -> println("1입니다")
+        2 -> println("2입니다")
+        3, 4, 5 -> println("3이나 4나 5입니다")
+        in 6..20 -> println("6부터 20 사이의 값입니다")
+        !in 8..10 -> println("")
+        else -> println("else")
+    }
+
+    val person = Person("홍길동")
 }
 
 fun myMethod(a: Int, b: Int): Int = a + b
+
+class Person(name: String) {
+    init{
+        println(name)
+    }
+}
